ff36f0cdfd2d72621268f9d5bd0b83e0537428f8
barbara-tshimologong <barbara@tshimologong.joburg>
2022-05-18 10:32:11 +0200

    Add project files.

---
+++ AFCentral/AFCentral.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess;
+
+namespace AFCentral.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+
+            var connectionString = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = AFCentral; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+
+            DataAccess.SqlServer ss = new DataAccess.SqlServer(connectionString);
+
+            var reply = ss.DbConnect();
+
+            System.Console.WriteLine(reply);
+
+
+        }
+    }
+}
---
+++ AFCentral/DataAccess/SqlServer.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SqlClient;
+
+namespace DataAccess
+{
+    public class SqlServer
+    {
+
+        private string _cnString;
+        public SqlServer(string cn)
+        {
+            _cnString = cn;
+        }
+        public string DbConnect()
+        {
+            return "you have connected.!!!";
+
+        }
+
+    }
+}
